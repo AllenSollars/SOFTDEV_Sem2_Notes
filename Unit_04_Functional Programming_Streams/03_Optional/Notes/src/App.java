fb4ae0ecf19dbbfd7b16485347301be5ffd41ba8
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-02-08 09:18:24 -0500

    updates

--- Unit_04_Functional Programming_Streams/03_Optional/Notes/src/App.java
+++ Unit_04_Functional Programming_Streams/03_Optional/Notes/src/App.java
@@ -1,5 +1,10 @@
+import java.util.Optional;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Optional<Integer> optInt = Optional.of(3);
+        Optional<String> result = optInt.flatMap((i) -> Optional.of(i.toString()));
+
     }
 }
