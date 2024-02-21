323152de2423c9a7145609d6373d728bbf867845
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-02-21 06:59:46 -0500

    updates

--- Unit_04_Functional Programming_Streams/06_Parallel Streams/Notes/src/App.java
+++ Unit_04_Functional Programming_Streams/06_Parallel Streams/Notes/src/App.java
@@ -4,11 +4,17 @@ import java.util.stream.IntStream;
 public class App {
     public static void main(String[] args) throws Exception {
         System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "4");
-        List<Integer> myInts = IntStream.rangeClosed(1, 1000).boxed().toList();
+        List<Integer> myInts = IntStream.rangeClosed(1, 100000000).boxed().toList();
         double startMillis = System.currentTimeMillis();
-        List<Double> newDoubles = myInts.stream().map((i) -> (double)2*i).toList();
+        myInts.stream().map((i) -> (double) 2 * i);
         double endMillis = System.currentTimeMillis();
         double difference = endMillis - startMillis;
+        System.out.println(difference);
+
+        startMillis = System.currentTimeMillis();
+       myInts.parallelStream().map((i) -> (double) 2 * i);
+        endMillis = System.currentTimeMillis();
+        difference = endMillis - startMillis;
         System.out.println(difference);
     }
 }
