750e54f0d01a336fefb1b4c4a6e201d07319d627
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-03-07 10:44:04 -0500

    updates

--- Unit_07_Miscellaneous/01_Reflection/Notes/src/App.java
+++ Unit_07_Miscellaneous/01_Reflection/Notes/src/App.java
@@ -1,5 +1,20 @@
+import java.lang.reflect.Method;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Person person1 = new Person("John", 24);
+        System.out.println(person1.getClass().getName());
+        Method[] person1Methods = person1.getClass().getMethods();
+        for(int i = 0; i < person1Methods.length; i++){
+            System.out.println(person1Methods[i].getName());
+            if(person1Methods[i].getName() == "getAge"){
+                person1Methods[i].invoke(person1);
+
+            }
+        }
+
+
+
+
     }
 }
